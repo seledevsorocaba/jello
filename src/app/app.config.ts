110cb2ebd50d3d03e4c8a3b4915bc33052bcbf8f
seledevsorocaba <selevsorocaba@gmail.com>
2026-08-04 22:19:46 -0300

    Auto-generated commit

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
-  ],
-};
